375ab2b984c6b19fa3e1c7b9c89289ffcee8981a
Mirandad10 <dmiranda0005@gmail.com>
2023-11-14 13:20:03 -0800

    finsihed server.js

---
+++ serever.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', api);
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname, '/public/index.html')
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`)
+});
